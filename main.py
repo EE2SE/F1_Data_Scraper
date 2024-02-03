5c0be44ff345171375da1c961245ce0748b58213
EE2SE <147542435+EE2SE@users.noreply.github.com>
2024-02-03 21:27:20 +0000

    got basic datastructure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # sort data into input features: for every race
 
@@ -22,30 +23,88 @@ import pandas as pd
 
 features_df = pd.DataFrame(columns=["raceId","driverId","circuitId",
                                     "constructorId","year","grid",
+                                    "finish",
                                     "q1time","q2time","q3time",
                                     "resultN1","resultN2","resultN3",
                                     "weather","circuitType",
                                     "generalClassification"])
 
+qualifying_df = pd.DataFrame();
+
 def run_main():
-    # global drivers_df
-    # global results_df
-    # global races_df
+    global features_df
+    global qualifying_df
     drivers_df = pd.read_csv("kaggle F1/drivers.csv")
     results_df = pd.read_csv("kaggle F1/results.csv")
     races_df = pd.read_csv("kaggle F1/races.csv")
     qualifying_df = pd.read_csv("kaggle F1/qualifying.csv")
 
-    # here we grab an ID of a race
-    for index_races, row_races in races_df[races_df.year == 2023].iterrows():
-        print("NEW RACE\n\n")
-        # for this race, we need to find all results
-        for index_res, row_res in results_df[results_df.raceId == row_races["raceId"]].iterrows():
-            print("started {:d}, finished {:s} ".format(row_res["grid"], row_res["position"]))
+    # populate the dataset with race results in those years
+    for year in range(2010,2024):
 
+        # here we grab an ID of a race from a particular year
+        for index_races, row_races in races_df[races_df.year == year].iterrows():
 
+            # make sure there are results from that race
+            if len(results_df[results_df.raceId == row_races["raceId"]]) != 0:
+
+                # for this race, we need to find all drivers finishing results
+                for index_res, row_res in results_df[results_df.raceId == row_races["raceId"]].iterrows():
+
+                    # we also need quali times
+                    try:
+                        q1_time = qualifying_df[(qualifying_df.raceId == row_races["raceId"]) & (qualifying_df.driverId == row_res["driverId"])]["q1"].iloc[0]
+                        q2_time = qualifying_df[(qualifying_df.raceId == row_races["raceId"]) & (qualifying_df.driverId == row_res["driverId"])]["q2"].iloc[0]
+                        q3_time = qualifying_df[(qualifying_df.raceId == row_races["raceId"]) & (qualifying_df.driverId == row_res["driverId"])]["q3"].iloc[0]
+                    except IndexError:
+                        q1_time = "\\N"
+                        q2_time = "\\N"
+                        q3_time = "\\N"
+
+                    if len(features_df[features_df.driverId == row_res["driverId"]]) > 2:
+                        result_n1 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-1]
+                        result_n2 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-2]
+                        result_n3 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-3]
+
+                    elif len(features_df[features_df.driverId == row_res["driverId"]]) > 1:
+                        result_n1 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-1]
+                        result_n2 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-2]
+                        result_n3 = "\\N"
+
+                    elif len(features_df[features_df.driverId == row_res["driverId"]]) > 0:
+                        result_n1 = features_df[features_df.driverId == row_res["driverId"]]["finish"].iloc[-1]
+                        result_n2 = "\\N"
+                        result_n3 = "\\N"
+
+                    else:
+                        result_n1 = "\\N"
+                        result_n2 = "\\N"
+                        result_n3 = "\\N"
+
+                    entry = {
+                        "raceId": row_races["raceId"],
+                        "driverId": row_res["driverId"],
+                        "circuitId": row_races["circuitId"],
+                        "constructorId": row_res["constructorId"],
+                        "year": year,
+                        "grid": row_res["grid"],
+                        "finish": row_res["position"],
+                        "q1time": q1_time,
+                        "q2time": q2_time,
+                        "q3time": q3_time,
+                        "resultN1": result_n1,
+                        "resultN2": result_n2,
+                        "resultN3": result_n3,
+                        "weather": 0,
+                        "circuitType": 0,
+                        "generalClassification": 0
+                    }
+
+                    entry_df = pd.DataFrame([entry])
+                    features_df = pd.concat([features_df, entry_df], ignore_index=True)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     run_main()
+    print(features_df.head())
